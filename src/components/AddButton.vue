ef1df0f803f94acff3d188c6d65a6d9aebb53983
Guitar_SPK <Supphakit.tar@mail.kmutt.ac.th>
2021-04-20 04:42:16 +0700

    add componet AddButton by local registation

---
+++ src/components/AddButton.vue
@@ -0,0 +1,22 @@
+<template>
+      <section v-show="!addEditState" class="flex items-center justify-center py-5">
+    <div class="lg:w-4/6">
+      <span class="p-2 px-auto">
+        <button @click="addEditTap" class="bg-green-400 font-semibold w-40 rounded-md">
+          <p class="py-2">Add Product</p>
+        </button>
+      </span>
+    </div>
+  </section>
+</template>
+<script>
+
+export default ({
+   props:['addEditState'],
+   methods:{
+    addEditTap(){
+        this.$emit('add-edit-tap')
+    }
+   }
+})
+</script>
